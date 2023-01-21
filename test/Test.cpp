f62a3b33f7c91666ee7e2bd4728046d99a22eff9
Evan Teran <evan.teran@gmail.com>
2023-01-21 14:31:26 -0500

    improved the example parser slightly to highlight a good way to handle keywords

--- test/Test.cpp
+++ test/Test.cpp
@@ -8,19 +8,26 @@ namespace {
 
 struct Token {
 	enum Type {
-		Identifier,
-		Number,
 		Delimeter,
+		Identifier,
+		Keyword,
+		Number,
 	} type;
 
 	std::string value;
 };
 
 const std::regex integer_regex(R"((0|[1-9][0-9]*))");
-const std::regex identifier_regex(R"([_a-zA-Z$][_a-zA-Z0-9]*)");
+const std::regex identifier_regex(R"([a-zA-Z_][a-zA-Z0-9_]*)");
 const std::regex whitespace_regex(R"([ \t\r\n]+|//.+)");
+const std::regex keyword_regex(R"(xor_eq|xor|while|wchar_t|volatile|void|virtual|using|unsigned|union|typename|typeid|typedef|try|true|throw|thread_local|this|template|switch|struct|static_cast|static_assert|static|sizeof|signed|short|return|requires|reinterpret_cast|register|public|protected|private|or_eq|or|operator|nullptr|not_eq|not|noexcept|new|namespace|mutable|long|int|inline|if|goto|friend|for|float|false|extern|export|explicit|enum|else|dynamic_cast|double|do|delete|default|decltype|continue|constinit|constexpr|consteval|const_cast|const|concept|compl|co_yield|co_return|co_await|class|char8_t|char32_t|char16_t|char|catch|case|break|bool|bitor|bitand|auto|asm|and_eq|and|alignof|alignas)");
 
 const char input[] = R"(
+/**
+ * @brief Entry point to the program
+ *
+ * @return int
+ */
 int main() {
 	// A C++ style comment
 	return /* A C style
@@ -58,7 +65,9 @@ void test_lex_cpp() {
 		}
 
 		// extract the next token...
-		if (auto ident = reader.match(identifier_regex)) {
+		if (auto keyword = reader.match(keyword_regex)) {
+			tokens.push_back({Token::Keyword, *keyword});
+		} else if (auto ident = reader.match(identifier_regex)) {
 			tokens.push_back({Token::Identifier, *ident});
 		} else if (auto integer = reader.match(integer_regex)) {
 			tokens.push_back({Token::Number, *integer});
@@ -79,7 +88,7 @@ void test_lex_cpp() {
 	}
 
 	// assert that we got what we expected..
-	assert(tokens[0].type == Token::Identifier);
+	assert(tokens[0].type == Token::Keyword);
 	assert(tokens[0].value == "int");
 
 	assert(tokens[1].type == Token::Identifier);
@@ -94,7 +103,7 @@ void test_lex_cpp() {
 	assert(tokens[4].type == Token::Delimeter);
 	assert(tokens[4].value == "{");
 
-	assert(tokens[5].type == Token::Identifier);
+	assert(tokens[5].type == Token::Keyword);
 	assert(tokens[5].value == "return");
 
 	assert(tokens[6].type == Token::Number);
